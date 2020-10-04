855cd1de55249d91cca41f5fb9853bd882601fa3
Kamil <isoyevkm@gmail.com>
2020-10-04 11:09:05 +0300

    task 1 is done

--- task-1-hw-7.js
+++ task-1-hw-7.js
@@ -1,1 +1,14 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const categories = document.querySelectorAll("li.item");
+
+  console.log(`В списке ${categories.length} категории`);
+
+  categories.forEach((item) => {
+    console.log(`Категория ${item.querySelector("h2").textContent}`);
+    console.log(
+      `Количество элементов: ${item.querySelector("ul").children.length}`
+    );
+  });
+});
